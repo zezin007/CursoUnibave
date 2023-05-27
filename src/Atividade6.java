6cd20722af3c573a69dc708f5322bc1c667ea2d1
zezin007 <79230406+zezin007@users.noreply.github.com>
2023-05-27 09:14:03 -0300

    Update Atividade6.java

--- src/Atividade6.java
+++ src/Atividade6.java
@@ -2,16 +2,18 @@ import javax.swing.JOptionPane;
 
 public class Atividade6 {
     public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog(null, "Digite um valor: ");
-        int num1 = Integer.parseInt(input);
-        String input1 = JOptionPane.showInputDialog(null, "Digite um valor: ");
-        int num2 = Integer.parseInt(input1);
+        public static void main(String[] args) {
+        public static void main (String[]args){
+            String input = JOptionPane.showInputDialog(null, "Digite um valor: ");
+            int num1 = Integer.parseInt(input);
+            String input1 = JOptionPane.showInputDialog(null, "Digite um valor: ");
+            int num2 = Integer.parseInt(input1);
 
-        if (num1 % num2 == 0 || num2 % num1 == 0) {
-            System.out.println("Os números são múltiplos.");
-        } else {
-            System.out.println("Os números não são múltiplos.");
+            if (num1 % num2 == 0 || num2 % num1 == 0) {
+                JOptionPane.showMessageDialog(null, "Os números são múltiplos.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Os números não são múltiplos.");
+            }
         }
     }
 }
-
