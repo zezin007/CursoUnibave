a761d761c25603b0390eea077bbc17fa0223681d
zezin007 <79230406+zezin007@users.noreply.github.com>
2023-05-27 09:41:28 -0300

    Create Atividade8.java

---
+++ src/Atividade8.java
@@ -0,0 +1,16 @@
+public class Atividade8 {
+    public static void main(String[] args) {
+        int numero = 0;
+        do {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+
+            numero++;
+        } while (numero <= 20);
+
+
+    }
+
+
+}
