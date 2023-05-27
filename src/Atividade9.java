12ea27c01da95b5b94b9430b1b54678e9896c58b
zezin007 <79230406+zezin007@users.noreply.github.com>
2023-05-27 10:00:45 -0300

    Create Atividade9.java

---
+++ src/Atividade9.java
@@ -0,0 +1,14 @@
+public class Atividade9 {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 10) {
+            if (numero == 7) {
+                numero++;
+
+            }
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
