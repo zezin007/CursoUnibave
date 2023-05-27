be8c50fd9eeff5c81e6ef7d3d0ac54097106c47b
zezin007 <79230406+zezin007@users.noreply.github.com>
2023-05-27 09:20:38 -0300

    Update Atividade5.java

--- src/Atividade5.java
+++ src/Atividade5.java
@@ -3,16 +3,17 @@ public class Atividade5 {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog(null, "Digite um valor: ");
         int num = Integer.parseInt(input);
-        
+
         if (num % 2 == 0){
-            System.out.println("Este número é Par");
+            JOptionPane.showMessageDialog(null, "Este número é Par");
         }
         else {
-            System.out.println("Este numero é Ímpar");
+            JOptionPane.showMessageDialog(null,"Este numero é Ímpar");
 
         }
     }
 }
+
 
 
         
